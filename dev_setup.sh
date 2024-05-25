118dd46ad18c7151a16a196d5612dbdb2633eb4b
dev <>
2024-05-24 20:56:57 -0400

    install chrome

--- dev_setup.sh
+++ dev_setup.sh
@@ -4,6 +4,13 @@ sudo apt update
 sudo apt-get upgrade -y
 
 
+# install chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+
 # Install programs / utils
 sudo apt-get install -y tilix
 sudo apt-get install -y git
